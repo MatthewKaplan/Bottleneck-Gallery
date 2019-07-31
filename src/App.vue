596b19eabb9a6d59da9864d7bc2064843ff8d816
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-30 20:50:37 -0600

    Fetch image data

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
     <Header />
+    <Images v-on:selected-image="selectedImage" />
+    <ImageModal v-bind:currentImage="currentImage" v-if="noSelectedImages" v-on:close-modal="closeModal"/>
   </div>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
+import Header from "./components/views/Header";
+import Images from "./components/views/Images";
+import ImageModal from "./components/views/ImageModal";
+import axios from "axios";
 
 export default {
   name: "app",
   components: {
-    Header
+    Header,
+    Images,
+    ImageModal
   },
   data() {
-    return {};
+    return {
+      currentImage: []
+    };
   },
-  methods: {}
+  methods: {
+    selectedImage(image) {
+      this.currentImage = [image];
+      console.log(this.currentImage)
+    },
+    closeModal() {
+      this.currentImage = []
+    }
+  },
+  computed: {
+    noSelectedImages() {
+      return this.currentImage.length >= 1
+    }
+  }
 };
 </script>
 
@@ -29,6 +51,11 @@ export default {
 body {
   font-family: Arial, Arial, Helvetica, sans-serif;
   line-height: 1.4;
+  background: url(assets/gallery.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 }
 
 .btn {

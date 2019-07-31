8ac3054839f60f313e560e5ac7d43b7851572d0b
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-31 01:40:24 -0600

    Created array with gallery data for slides

---
+++ src/helper/data.js
@@ -0,0 +1,126 @@
+const galleryArray = [
+  {
+    name: "Modern and Contemporary Art",
+    theme: "Social Realism",
+    id: 1320,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:75998_dynmc"
+  },
+  {
+    name: "Early Chinese Art",
+    theme: "Arts of Ancient China from the Neolithic to the Bronze Age",
+    id: 1740,
+    image: "https://nrs.harvard.edu/urn-3:huam:DDC112427_dynmc"
+  },
+  {
+    name: "European and American Art, 17th–19th century",
+    theme: "The Silver Cabinet: Art and Ritual, 1600–1850",
+    id: 2340,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:VRS14203_dynmc"
+  },
+  {
+    name: "Ancient Mediterranean and Near Eastern Art",
+    theme: "Classical Sculpture",
+    id: 3200,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:756755"
+  },
+  {
+    name: "Ancient Mediterranean and Near Eastern Art",
+    theme: "Ancient Egypt: Art for Eternity",
+    id: 3740,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:INV011632_dynmc"
+  },
+  {
+    name: "Modern and Contemporary Art",
+    theme: "Art in Germany Between the Wars",
+    id: 1520,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:LEG273767"
+  },
+  {
+    name: "Modern and Contemporary Art",
+    theme: "The Sixties Experiment/Multiple Strategies",
+    id: 1100,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC244506_dynmc"
+  },
+  {
+    name: "European Art, 19th–20th century",
+    theme: "Collection of Maurice Wertheim, Class of 1906",
+    id: 1220,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC252060_dynmc"
+  },
+  {
+    name: "European and American Art, 17th–19th century",
+    theme: "Centuries of Tradition, Changing Times: Art for an Uncertain Age",
+    id: 2100,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:770509"
+  },
+  {
+    name: "European and American Art, 17th–19th century",
+    theme: "Rococo and Neoclassicism in the Eighteenth Century",
+    id: 2220,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC252612_dynmc"
+  },
+  {
+    name: "Ancient Mediterranean and Near Eastern Art",
+    theme: "Ancient Near Eastern Art in the Service of Kings",
+    id: 3440,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:VRS10353_dynmc"
+  },
+  {
+    name: "Art from Islamic Lands",
+    theme: "The Middle East and North Africa",
+    id: 2550,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC252363_dynmc"
+  },
+  {
+    name: "Ancient Mediterranean and Near Eastern Art",
+    theme: "Roman Art",
+    id: 3700,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC231389_dynmc"
+  },
+  {
+    name: "Medieval Art",
+    theme: "Medieval Times Art",
+    id: 2440,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:73150_dynmc"
+  },
+  {
+    name: "European Art, 13th–16th century",
+    theme: "Art and Image in Europe",
+    id: 2500,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC252603_dynmc"
+  },
+  {
+    name: "European Art, 13th–16th century",
+    theme: "The Renaissance",
+    id: 2540,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC231549_dynmc"
+  },
+  {
+    name: "East Asian Art",
+    theme: "East Asian Painting and Decorative Arts",
+    id: 2600,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:73727_dynmc"
+  },
+  {
+    name: "Ancient Mediterranean and Near Eastern Art",
+    theme: "Ancient Greece in Black and Orange",
+    id: 3400,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:DDC102627_dynmc"
+  },
+  {
+    name: "Early Chinese Art",
+    theme: "Arts of Ancient China from the Bronze Age to the Golden Age",
+    id: 1600,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:61515_dynmc"
+  },
+  {
+    name: "Buddhist Sculpture",
+    theme: "Buddhism and Early East Asian Buddhist Art",
+    id: 1610,
+    image: "https://nrs.harvard.edu/urn-3:HUAM:30679_dynmc"
+  }
+]
+
+module.exports = {
+  galleryArray
+}
